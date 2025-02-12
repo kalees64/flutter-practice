c7edc4669c78e6b4d6b678271c67a55de46a33ef
Kaleeswaran V <vkalees64@gmail.com>
2025-02-12 18:14:27 +0530

    feat: bottom navigation bar added

--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "VK64",
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("VK64"),
+        appBar: AppBar(
+          title: Text(
+            "VK64",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
           ),
-          body: Column(
-            children: [
-              h1(text: "My first Flutter app with custom H1 component"),
-              h2(text: "My first Flutter app with custom H2 component"),
-              h3(text: "My first Flutter app with custom H3 component"),
-              h4(text: "My first Flutter app with custom H4 component"),
-              h5(text: "My first Flutter app with custom H5 component"),
-            ],
-          )),
+          backgroundColor: Colors.blue,
+        ),
+        body: Column(
+          children: [
+            h1(text: "My first Flutter app with custom H1 component"),
+            h2(text: "My first Flutter app with custom H2 component"),
+            h3(text: "My first Flutter app with custom H3 component"),
+            h4(text: "My first Flutter app with custom H4 component"),
+            h5(text: "My first Flutter app with custom H5 component"),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_balance), label: "Account"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: "Settings"),
+          ],
+          backgroundColor: Colors.blue,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.black,
+          type: BottomNavigationBarType.fixed,
+          onTap: (index) {
+            print("I Love You $index");
+          },
+        ),
+      ),
     );
   }
 }
