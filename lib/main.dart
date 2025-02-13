2885ce393b337a2a2c3a233953ba58498065c833
Kaleeswaran V <vkalees64@gmail.com>
2025-02-13 16:06:34 +0530

    feat: product card widget added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:first_flutter_app/src/common/widgets/customHeadings.dart';
+import 'package:first_flutter_app/src/common/widgets/productCardWidget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,14 +21,17 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.blue,
         ),
-        body: Column(
-          children: [
-            h1(text: "My first Flutter app with custom H1 component"),
-            h2(text: "My first Flutter app with custom H2 component"),
-            h3(text: "My first Flutter app with custom H3 component"),
-            h4(text: "My first Flutter app with custom H4 component"),
-            h5(text: "My first Flutter app with custom H5 component"),
-          ],
+        body: Container(
+          padding: EdgeInsets.all(7),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 4,
+            children: [
+              h5(text: "Product Widgets"),
+              productCard(),
+              productCard(),
+            ],
+          ),
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: [
@@ -43,7 +47,7 @@ class MyApp extends StatelessWidget {
           unselectedItemColor: Colors.black,
           type: BottomNavigationBarType.fixed,
           onTap: (index) {
-            print("I Love You $index");
+            print("Index is $index");
           },
         ),
       ),
---
+++ lib/src/common/widgets/productCardWidget.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+Widget productCard() {
+  return Row(
+    children: [
+      Expanded(
+        child: Container(
+          padding: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+              color: Colors.green, borderRadius: BorderRadius.circular(5)),
+          child: Row(
+            spacing: 8,
+            children: [
+              Container(
+                width: 75,
+                height: 75,
+                padding: EdgeInsets.all(3),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(2)),
+                child: Placeholder(
+                  color: Colors.green,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  height: 75,
+                  alignment: Alignment.topLeft,
+                  padding: EdgeInsets.all(4),
+                  child: Column(
+                    spacing: 4,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Product 1",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ],
+  );
+}
